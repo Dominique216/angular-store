3e43abb6de4ad455a8853ca276f18279127bd315
Dominique Nix <raquel_0216@yahoo.com>
2023-05-02 18:17:10 -0500

    static homepage complete

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: {[id:number]: number} = {
+  1: 400,
+  3: 335, 
+  4: 350
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
 
   constructor() { }
@@ -14,7 +21,8 @@ export class HomeComponent implements OnInit {
   }
 
   onColumnsCountChange(colsNum: number):void {
-    this.cols = colsNum
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols]
   }
 
   onShowCategory(newCategory: string):void{
